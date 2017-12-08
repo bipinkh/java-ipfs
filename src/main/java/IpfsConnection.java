f03d529620145426baef7e153e2a3cb99860a3f8
bipinkh <bipinkh91@gmail.com>
2017-12-08 13:09:38 +0545

    mp3 upload ok. working on folder upload

--- src/main/java/IpfsConnection.java
+++ src/main/java/IpfsConnection.java
@@ -5,11 +5,14 @@ import io.ipfs.multihash.Multihash;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class IpfsConnection {
 
-    //all files need to be inside res/files directory
+    //all files and folder needs to be inside res/files directory
     private static final String filename = "bipin.txt";
+    private static final String mp3filename = "MileyCyrus.mp3";
+    private static final String directoryName = "bipinDir";
     private static byte[] content = "This is just a random line copied from somewhere to check the demo.".getBytes();
 
     private static IPFS ipfs;
@@ -19,7 +22,7 @@ public class IpfsConnection {
          ipfs.refs.local();
 
          //file uplaod and view
-        String filehash = addFileToIPFS(filename); //Qmd1GGzoLtzkZXuJ29i87fX2NQfcv8pWLZJ8BBgVXzRJbA
+        String filehash = addFileToIPFS(filename);
         System.out.println("File Uploaded. Identifier Hash = " + filehash);
         displayFileFromIPFS(filehash);
 
@@ -27,6 +30,14 @@ public class IpfsConnection {
         String bytehash = addRawDataToIPFS(content);
         System.out.println("Raw bytes uploaded. Identifier Hash = " + filehash);
         displayFileFromIPFS(bytehash);
+
+        //mp3 upload
+        String mp3filehash = addFileToIPFS(mp3filename);
+        System.out.println("MP3 Uploaded. Identifier Hash = " + mp3filehash);
+
+        //directory upload
+        String folderhash = addFolderToIPFS(directoryName);
+        System.out.println("Directory Uploaded. Identifier Hash = " + folderhash);
 
     }
 
@@ -47,16 +58,18 @@ public class IpfsConnection {
         return pointer.toString();
     }
 
-    
     public static void displayFileFromIPFS(String filehash) throws IOException {
         Multihash filePointer = Multihash.fromBase58(filehash);
         byte[] fileContents = ipfs.cat(filePointer);
-        System.out.println("\n \n File contents ::\n"+new String(fileContents));
+        System.out.println("File contents :: "+new String(fileContents));
     }
 
 
-
-
-
+    public static String addFolderToIPFS(String foldername) throws IOException {
+        NamedStreamable.DirWrapper dir = new NamedStreamable.DirWrapper(".res/files/"+foldername, Arrays.<NamedStreamable>asList());
+        MerkleNode addResult = ipfs.add(dir).get(0);
+        Multihash pointer = addResult.hash;
+        return pointer.toString();
+    }
 
 }
