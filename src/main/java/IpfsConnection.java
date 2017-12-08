d94e78a9fcd005fd8aa27b2e7b3e738b73d54f7b
bipinkh <bipinkh91@gmail.com>
2017-12-08 12:37:24 +0545

    raw bytes addition and view

--- src/main/java/IpfsConnection.java
+++ src/main/java/IpfsConnection.java
@@ -10,6 +10,7 @@ public class IpfsConnection {
 
     //all files need to be inside res/files directory
     private static final String filename = "bipin.txt";
+    private static byte[] content = "This is just a random line copied from somewhere to check the demo.".getBytes();
 
     private static IPFS ipfs;
 
@@ -17,13 +18,18 @@ public class IpfsConnection {
          ipfs = new IPFS("/ip4/127.0.0.1/tcp/5001");
          ipfs.refs.local();
 
-         //file uplaod
+         //file uplaod and view
         String filehash = addFileToIPFS(filename); //Qmd1GGzoLtzkZXuJ29i87fX2NQfcv8pWLZJ8BBgVXzRJbA
         System.out.println("File Uploaded. Identifier Hash = " + filehash);
-        //ipfs file content display
         displayFileFromIPFS(filehash);
-        
+
+        //raw bytes upload and view
+        String bytehash = addRawDataToIPFS(content);
+        System.out.println("Raw bytes uploaded. Identifier Hash = " + filehash);
+        displayFileFromIPFS(bytehash);
+
     }
+
 
     public static String addFileToIPFS(String filename) throws IOException {
         NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File("./res/files/"+filename));
@@ -32,11 +38,25 @@ public class IpfsConnection {
         return pointer.toString();
     }
 
+
+    public static String addRawDataToIPFS(byte[] content) throws IOException {
+        String name = "hello.txt";
+        NamedStreamable.ByteArrayWrapper file = new NamedStreamable.ByteArrayWrapper(name, content);
+        MerkleNode addResult = ipfs.add(file).get(0);
+        Multihash pointer =  addResult.hash;
+        return pointer.toString();
+    }
+
+    
     public static void displayFileFromIPFS(String filehash) throws IOException {
         Multihash filePointer = Multihash.fromBase58(filehash);
         byte[] fileContents = ipfs.cat(filePointer);
         System.out.println("\n \n File contents ::\n"+new String(fileContents));
     }
+
+
+
+
 
 
 }
